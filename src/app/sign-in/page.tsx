920546ba83cc5f3199d2561e572c855be2707ee7
nethmesaboth <nethmesaboth10@gmail.com>
2025-06-25 09:19:49 +0700

    adding shadcn ui component

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+const Page = () => {
+	return (
+		<div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+			<div className="w-full max-w-sm md:max-w-3xl">
+				<Card>Sign in page</Card>
+			</div>
+		</div>
+	);
+};
+
+export default Page;
